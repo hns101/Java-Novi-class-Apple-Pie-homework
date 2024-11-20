9d35a6bbb62d40c4dade4edb51e93fd1e9510ae0
hns101 <hns101@live.nl>
2024-11-20 16:56:10 +0100

    huiswerk week 2backend

---
+++ src/Ingredient.java
@@ -0,0 +1,22 @@
+public class Ingredient {
+
+    double amount;
+    String unit;
+    String name;
+
+    public Ingredient(double amount, String unit, String name) {
+        this.amount = amount;
+        this.unit = unit;
+        this.name = name;
+    }
+
+    public Ingredient(){
+
+    }
+
+    @Override
+    public String toString() {
+        return amount + " " + unit + " " + name;
+
+    }
+}
---
+++ src/Main.java
@@ -0,0 +1,15 @@
+public class Main {
+
+    public static void main(String[] args) {
+
+        ApplePieRecipe applePieRecipe = new ApplePieRecipe();
+
+        applePieRecipe.printIngredients();
+        applePieRecipe.printSteps();
+
+
+
+
+    }
+
+}
